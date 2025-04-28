335b04f9dbf71fe5e66c27dd65be471947744492
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2025-04-29 02:25:13 +0600

    first commit of development

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
-import app from "./app"
 import mongoose from "mongoose";
+import app from "./app";
 import env from "./utils/validateEnv";
 
 const port = env.PORT;
 
-mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
-  console.log("Mongoose connected!");
-  app.listen(port, () => {
-    console.log("Server is running on port: " + port);
-  });
-}).catch(console.error);
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected!");
+    app.listen(port!, () => {
+      console.log("Server is running on port: " + port);
+    });
+  })
+  .catch(console.error);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button className="bg-gray " onClick={}>Click me</Button>
+      <Button className="bg-gray">Click me</Button>
     </>
   );
 }
