0f5d343fd7f8b1bb9a19501c9e8a4c60373633bf
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2025-04-29 02:31:14 +0600

    experiment on main branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button className="bg-gray">Click me</Button>
     </>
   );
 }
