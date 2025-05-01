eddc68779574621fed3ff6e47bff70ed26d874bc
Izhaan Kader <izhaankader@gmail.com>
2025-05-01 17:14:07 +0600

    hello guys

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h2>My name is izhaan. And I am the second developer</h2>
+      <h3>Hello guys!!</h3>
     </>
   );
 }
